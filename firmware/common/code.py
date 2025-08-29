687e4c77f49c3733a4f6251aa73303c86baf940a
bobthechemist <bobthechemist@gmail.com>
2025-08-29 12:10:34 -0400

    updated file structure

--- firmware/common/code.py
+++ firmware/common/code.py
@@ -1,7 +1,7 @@
 # type: ignore
 # Basic setup for code.py. Replace SUBSYSTEM with the appropriate one
 
-from blueprint.subsystems.SUBSYSTEM import machine
+from firmware.SUBSYSTEM import machine
 from time import sleep
 
 machine.run()
